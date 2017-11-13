ececbde2a75d1680ff82f286fa5a8eef9157cd87
Simon Benoit <spiky1987@hotmail.com>
2017-11-12 19:38:13 -0500

    Update python script with global variables

--- src/testSerial.py
+++ src/testSerial.py
@@ -3,41 +3,55 @@ import time
 
 s = serial.Serial(port = "COM3", baudrate = "115200")
 
+updateCmd = (0x2B,0,0,0,0,0)
+delay = 0.04
+
 while 1:
-    for i in range (0, 30):
-        payload = (0x3d,0x00,i,0x0,0x0,8*i)
-        s.write(payload)
-        payload = (0x3d,0x00,35-i,0x0,0x0,8*i)
-        s.write(payload)
-        payload = (0x2B,0,0,0,0,0)
-        s.write(payload)
-        time.sleep(0.02)
-    for i in range (0, 30):
-        payload = (0x3d,0x00,i,0x0,8*i,0)
-        s.write(payload)
-        payload = (0x3d,0x00,35-i,0x0,8*i,0x0)
-        s.write(payload)
-        payload = (0x2B,0,0,0,0,0)
-        s.write(payload)
-        time.sleep(0.02)
-    for i in range (0, 30):
-        payload = (0x3d,0x00,i,8*i,0,0)
-        s.write(payload)
-        payload = (0x3d,0x00,35-i,8*i,0x0,0)
-        s.write(payload)
-        payload = (0x2B,0,0,0,0,0)
-        s.write(payload)
-        time.sleep(0.02)
-    for i in range (0, 30):
-        payload = (0x3d,0x00,i,0x0,8*i,8*i)
-        s.write(payload)
-        payload = (0x2B,0,0,0,0,0)
-        s.write(payload)
-        time.sleep(0.02)
-    for i in range (0, 30):
-        payload = (0x3d,0x00,i,8*i,8*i,0)
-        s.write(payload)
-        payload = (0x2B,0,0,0,0,0)
-        s.write(payload)
-        time.sleep(0.02)
+    for power in range (1, 5):
+    
+        for i in range (0, 30):
+            intensity = power*i
+            color = 0x0,0x0,intensity
+            payload = (0x3d,0x00,i)
+            s.write(payload)
+            s.write(color)
+            payload = (0x3d,0x00,35-i)
+            s.write(payload)
+            s.write(color)
+            s.write(updateCmd)
+            time.sleep(delay)
+        for i in range (0, 30):
+            intensity = power*i
+            color = 0x0,intensity,0x0
+            payload = (0x3d,0x00,i)
+            s.write(payload)
+            s.write(color)
+            payload = (0x3d,0x00,35-i)
+            s.write(payload)
+            s.write(color)
+            s.write(updateCmd)
+            time.sleep(delay)
+        for i in range (0, 30):
+            intensity = power*i
+            color = intensity,0x0,0x0
+            payload = (0x3d,0x00,i)
+            s.write(payload)
+            s.write(color)
+            payload = (0x3d,0x00,35-i)
+            s.write(payload)
+            s.write(color)
+            s.write(updateCmd)
+            time.sleep(delay)
+        for i in range (0, 30):
+            intensity = power*i
+            color = 0x0,intensity,intensity
+            payload = (0x3d,0x00,i)
+            s.write(payload)
+            s.write(color)
+            payload = (0x3d,0x00,35-i)
+            s.write(payload)
+            s.write(color)
+            s.write(updateCmd)
+            time.sleep(delay)
+        
         
